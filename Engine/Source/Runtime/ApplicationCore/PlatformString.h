1b67b5075f94c22a51b4bddd540c51771306b1df
Will Belcher <will@tensorworks.com.au>
2025-04-09 01:19:21 +1000

    Add dummy platform string implementation for non-implemented platforms

--- Engine/Source/Runtime/ApplicationCore/PlatformString.h
+++ Engine/Source/Runtime/ApplicationCore/PlatformString.h
@@ -4,4 +4,26 @@
 	#include "Windows/WindowsPlatformString.h"
 #else
 	#include "Generic/GenericPlatformString.h"
+
+// clang-format off
+	struct DummyPlatformString : public GenericPlatformString
+	{
+		static size_t Strlen(const WIDECHAR* Str)
+		{
+			static_assert(false, "DummyPlatformString Strlen(const WIDECHAR* Str) is not implemented!");
+		}
+
+		static size_t Strlen(const ANSICHAR* Str)
+		{
+			static_assert(false, "DummyPlatformString Strlen(const ANSICHAR* Str) is not implemented!");
+		}
+
+		static size_t Strlen(const UTF8CHAR* Str)
+		{
+			static_assert(false, "DummyPlatformString Strlen(const UTF8CHAR* Str) is not implemented!");
+		}
+	}
+
+	typedef DummyPlatformString PlatformString;
+// clang-format on
 #endif
